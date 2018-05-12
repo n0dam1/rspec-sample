eb00fea3ff8c0bc020470c6885777c75fc601d52
n0dam1 <minoru.noda.0527@gmail.com>
2018-05-12 17:08:19 +0900

    FactoryBotの修正

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,8 +1,21 @@
 FactoryBot.define do
   factory :article do
-    title "MyText"
-    body "MyText"
-    status 1
-    published_at "2018-05-11 16:49:33"
+    title "遅延評価について"
+    body { "#{title}の記事です" }
+    status :draft
+
+    trait :published do
+      status :published
+    end
+
+    trait :long_title do
+      title 'とても長いタイトルを持った記事を作る属性'
+    end
+
+    trait :test_callback do
+      after(:create) do
+        p 'コールバックのテスト'
+      end
+    end
   end
 end
